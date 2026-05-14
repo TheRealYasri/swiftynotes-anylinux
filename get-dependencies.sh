5b3ddae66f61d1403714cb5d0d1376346a3b7aad
TheRealYasri <myoung1985@gmail.com>
2026-05-14 01:51:00 -0500

    Initial commit

---
+++ get-dependencies.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+set -eu
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+sudo pacman -Syy --noconfirm archlinux-keyring
+#Make
+sudo pacman -S --noconfirm --needed git base-devel
+#Needed
+sudo pacman -S --noconfirm --needed gtk4 libadwaita gtksourceview5 hunspell ncurses libxml2-legacy libspelling hunspell-en_us hunspell-en_gb hunspell-en_au hunspell-en_ca hunspell-de hunspell-fr hunspell-es_es hunspell-es_any hunspell-it hunspell-nl hunspell-pl hunspell-ro hunspell-ru hunspell-el hunspell-hu
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano
+
+echo "Installing swift building packages..."
+echo "---------------------------------------------------------------"
+
+sudo ln -s /usr/lib/libncursesw.so.6 /usr/lib/libncurses.so.6
+
+SWIFT_VERSION="6.2"
+if [ "$ARCH" = "aarch64" ]; then
+    URL="https://download.swift.org/swift-${SWIFT_VERSION}-release/ubuntu2204-aarch64/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-ubuntu22.04-aarch64.tar.gz"
+else
+    URL="https://download.swift.org/swift-${SWIFT_VERSION}-release/ubuntu2204/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-ubuntu22.04.tar.gz"
+fi
+if [ ! -d "/opt/swift" ]; then
+    echo "Downloading official Swift toolchain for $ARCH..."
+    curl -L "$URL" -o swift.tar.gz
+    sudo mkdir -p /opt/swift
+    sudo tar -xzf swift.tar.gz -C /opt/swift --strip-components=1
+fi
+export PATH="/opt/swift/usr/bin:$PATH"
+
+echo "Installing swiftynotes from source packages..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/makoni/swifty-notes-gtk.git source
+cd source
+mkdir -p Sources/CSpelling/include
+cp /usr/include/libspelling-1/libspelling.h Sources/CSpelling/include/
+export SWIFT_FLAGS="-Xcc -I$(pwd)/Sources/CSpelling/include"
+chmod +x packaging/release/assemble-install-root.sh
+./packaging/release/assemble-install-root.sh --prefix /usr --dest build-root
+sudo cp -rv build-root/usr/* /usr/
+cd ..
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
---
+++ make-appimage.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -eu
+
+# Setup
+ARCH=$(uname -m)
+export ARCH VERSION
+export OUTPATH=./dist
+export VERSION=$(grep 'version:' source/snap/snapcraft.yaml | head -n 1 | cut -d'"' -f2 | tr -d ' ')
+export ADD_HOOKS="self-updater.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/scalable/apps/me.spaceinbox.swiftynotes.svg
+export DESKTOP=/usr/share/applications/me.spaceinbox.swiftynotes.desktop
+
+# Deploy dependencies
+quick-sharun \
+    /usr/bin/swiftynotes \
+    /usr/libexec/swifty-notes/swiftynotes \
+    /usr/share/hunspell/ \
+    /usr/lib/libspelling-1.so* \
+    /usr/lib/libgtk-4.so* \
+    /usr/lib/libadwaita-1.so* \
+    /usr/lib/libgtksourceview-5.so* \
+    /usr/lib/libenchant-2.so* \
+    /usr/lib/libhunspell-1.7.so* \
+    /usr/lib/libxml2.so.2* \
+    /usr/lib/libncursesw.so.6*
+
+# Additional changes can be done in between here
+cp -r /usr/libexec/swifty-notes/swifty-notes-gtk_SwiftyNotes.resources AppDir/bin/
+rm -f AppDir/shared/lib/enchant-2/enchant_aspell.so
+rm -f AppDir/shared/lib/enchant-2/enchant_hspell.so
+rm -f AppDir/shared/lib/enchant-2/enchant_nuspell.so
+rm -f AppDir/shared/lib/enchant-2/enchant_voikko.so
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage --version "$VERSION" --output "."
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead#quick-sharun --test ./*.AppImage
