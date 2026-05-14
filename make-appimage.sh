e3b6e9da67f09f75fa84385dc79b53e6a27908fb
TheRealYasri <myoung1985@gmail.com>
2026-05-14 01:51:00 -0500

    Initial commit

---
+++ make-appimage.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+
+# Setup
+export VERSION=$(grep 'version:' source/snap/snapcraft.yaml | head -n 1 | cut -d'"' -f2 | tr -d ' ')
+echo "X-AppImage-Version=$VERSION" > ./appinfo
+
+cp /usr/libexec/swifty-notes/swiftynotes ./swiftynotes
+cp -r /usr/libexec/swifty-notes/swifty-notes-gtk_SwiftyNotes.resources ./swifty-notes-gtk_SwiftyNotes.resources
+
+export ADD_HOOKS="self-updater.hook"
+export OUTPATH="$(pwd)/dist"
+mkdir -p "$OUTPATH"
+
+export ICON=/usr/share/icons/hicolor/scalable/apps/me.spaceinbox.swiftynotes.svg
+export DESKTOP=/usr/share/applications/me.spaceinbox.swiftynotes.desktop
+
+# Deploy dependencies
+quick-sharun \
+    --exec "swiftynotes" \
+    ./swiftynotes \
+    ./swifty-notes-gtk_SwiftyNotes.resources \
+    /usr/lib/libgtk-4.so \
+    /usr/lib/libadwaita-1.so \
+    /usr/lib/libgtksourceview-5.so \
+    /usr/lib/libspelling-1.so \
+    /usr/lib/libenchant-2.so \
+    /usr/lib/enchant-2/ \
+    /usr/lib/libhunspell-1.7.so \
+    /usr/lib/libxml2.so.2 \
+    /usr/lib/libncursesw.so.6
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage --version "$VERSION" --output "$OUTPATH"
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test "$OUTPATH"/*.AppImage
