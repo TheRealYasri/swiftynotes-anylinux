94da092988b5b27c67c72b57dd7b4f85aa4d1ee2
TheRealYasri <myoung1985@gmail.com>
2026-05-14 08:07:56 -0500

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,27 +4,27 @@ set -eu
 # Setup
 export VERSION=$(grep 'version:' source/snap/snapcraft.yaml | head -n 1 | cut -d'"' -f2 | tr -d ' ')
 echo "X-AppImage-Version=$VERSION" > ./appinfo
-
-mkdir -p install_root
-./packaging/release/assemble-install-root.sh --dest install_root --prefix /usr
-
+sudo cp /usr/libexec/swifty-notes/swiftynotes /usr/bin/swiftynotes
+sudo cp -r /usr/libexec/swifty-notes/swifty-notes-gtk_SwiftyNotes.resources /usr/bin/
+sudo mkdir -p /usr/share/hunspell/
 export ADD_HOOKS="self-updater.hook"
 export OUTPATH="$(pwd)/dist"
 mkdir -p "$OUTPATH"
-export ICON=install_root/usr/share/icons/hicolor/scalable/apps/me.spaceinbox.swiftynotes.svg
-export DESKTOP=install_root/usr/share/applications/me.spaceinbox.swiftynotes.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/me.spaceinbox.swiftynotes.svg
+export DESKTOP=/usr/share/applications/me.spaceinbox.swiftynotes.desktop
 
 # Deploy dependencies
 quick-sharun \
     --exec "swiftynotes" \
-    install_root/usr/bin/swiftynotes \
-    install_root/usr/bin/*.resources \
+    /usr/bin/swiftynotes \
+    /usr/bin/swifty-notes-gtk_SwiftyNotes.resources \
     /usr/lib/libspelling-1.so \
     /usr/lib/libgtk-4.so \
     /usr/lib/libadwaita-1.so \
     /usr/lib/libgtksourceview-5.so \
     /usr/lib/libenchant-2.so \
-    /usr/lib/hunspell/ \
+    /usr/lib/libhunspell-1.7.so \
+    /usr/share/hunspell/ \
     /usr/lib/libxml2.so.2 \
     /usr/lib/libncursesw.so.6
 
